2bded71f77d46d1586cc4664f755ff510bdb4bbb
oyosc <chenlei@szzbmy.com>
2016-10-31 17:00:00 +0800

    add node-schedule

--- automatic/AutoClimbing.js
+++ automatic/AutoClimbing.js
@@ -1,30 +1,27 @@
 /**
  * Created by Administrator on 2016/10/31.
  */
+var schedule = require('node-schedule');
 var autoSpider = require('../service/autoSpider');
-var autoClimbing = function(isAuto){
-    var nowTime = new Date();
-    var hours = nowTime.getHours();
-    var minutes = nowTime.getMinutes();
-    var seconds = nowTime.getSeconds();
-    var time = hours + '-' + minutes + '-'+ seconds;
-    if(hours === '8-0-0'){
+
+
+function autoClimbing(isAuto){
+    schedule.scheduleJob({hour: 10, minute: 00}, function(){
         autoSpider(isAuto, function(err, result){
             if(err){
                 console.log('----------------Error------------------');
                 console.log('autoClimbing failed');
             }
+            else if(result.status === "notCorrect"){
+                console.log('----------------Warning------------------');
+                console.log('autoClimbing not open');
+            }
             else if(result){
                 console.log('autoClimbing success');
             }
         })
-    }else{
-        autoSpider(false, function(err, result){
-            if(result.status === "notCorrect"){
-                console.log('----------------Warning------------------');
-                console.log('now autoClimbing not correct-time');
-            }
-        })
-    }
+    });
+    
 }
+
 exports = module.exports = autoClimbing;
