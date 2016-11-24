654536b2f10e270ce9571ad3ff6c2eb6453abbad
oyosc <chenlei@szzbmy.com>
2016-11-24 17:21:49 +0800

    fix

--- timeTask/timeTask.js
+++ timeTask/timeTask.js
@@ -3,38 +3,38 @@
  */
 //只支持linux平台，后面会按照另一种思路来进行解决
 'use strict';
-const exec = require('child_process').exec;
+// const exec = require('child_process').exec;
 var dateParse = require('./dateParse');
 var TIMEOUT_MAX = 2147483647; // 2^31-1
 
-function timeTask(params, callback){
-    var startTime = ['*','*','*','*','*'];
-    var cronThing;
-    if(params.minute){
-        startTime[0] = params.minute;
-    }
-    if(params.hour){
-        startTime[1] = params.hour+1;
-    }
-    if(params.day){
-        startTime[2] = params.day;
-    }
-    if(params.month){
-        startTime[3] = params.month;
-    }
-    if(params.week){
-        startTime[4] = params.week;
-    }
-    cronThing = startTime.join(" ") + 'root' + params.order;
-    exec(cronThing, function(error, stdout, stderr){
-        if(error){
-            console.error('exec error: '+ error);
-            return;
-        }
-        console.log('stdout:'+ stdout)
-        console.log('stderr:' + stderr);
-    });
-}
+// function timeTask(params, callback){
+//     var startTime = ['*','*','*','*','*'];
+//     var cronThing;
+//     if(params.minute){
+//         startTime[0] = params.minute;
+//     }
+//     if(params.hour){
+//         startTime[1] = params.hour+1;
+//     }
+//     if(params.day){
+//         startTime[2] = params.day;
+//     }
+//     if(params.month){
+//         startTime[3] = params.month;
+//     }
+//     if(params.week){
+//         startTime[4] = params.week;
+//     }
+//     cronThing = startTime.join(" ") + 'root' + params.order;
+//     exec(cronThing, function(error, stdout, stderr){
+//         if(error){
+//             console.error('exec error: '+ error);
+//             return;
+//         }
+//         console.log('stdout:'+ stdout)
+//         console.log('stderr:' + stderr);
+//     });
+// }
 
 
 //采用另一种思路的定时任务
